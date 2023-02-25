3c714b2c98b5e6a9c3d6c07ce52eef39315c987d
andrewguest <andrew@aguest.me>
2023-02-24 18:55:45 -0600

    fix: correct API URL

--- backend/routes/dog_facts.py
+++ backend/routes/dog_facts.py
@@ -10,6 +10,6 @@ router = APIRouter(tags=["Dog facts"])
 @router.get("/dogfacts")
 async def dog_facts(facts_limit: Optional[int] = 10):
     response = httpx.get(
-        "http://dog-api.kinduff.com/api/facts", params={"number": facts_limit}
+        "https://dog-api.kinduff.com/api/facts", params={"number": facts_limit}
     )
     return {"Dog facts": response.json()["facts"]}
